c84797ff10fb714d3453e35f3eae9c0586a37cac
Misa <dieuhuongngo@gmail.com>
2020-04-19 21:53:37 +0300

    add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,18 +2,27 @@ import { Recipe } from './recipe-list/recipe.model';
 import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>()
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg'
+      'Proper Meal for Lunch',
+      'Shall we have some cheese today?',
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg',
+      [
+        { name: 'Cheese', amount: 1 },
+        { name: 'Oregano', amount: 0.5 }
+      ]
     ),
     new Recipe(
-      'Testest Recipe',
-      'Another test',
-      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg'
+      'Dessert recipe',
+      'Vitamin C for everyone',
+      'https://www.micadeli.com/wp-content/uploads/2018/11/tips-nemme-snacks.jpg',
+      [
+        { name: 'Kiwi', amount: 2 },
+        { name: 'Grapefruit', amount: 0.5 },
+        { name: 'Peach', amount: 1 }
+      ]
     )
   ];
 
