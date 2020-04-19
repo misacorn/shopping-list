61875891046ad14ac034de23c647b06108f878a4
Misa <dieuhuongngo@gmail.com>
2020-04-19 22:21:48 +0300

    inject service to service (shoppingList to recipe)

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe-list/recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-details',
@@ -9,7 +10,13 @@ import { Recipe } from '../recipe-list/recipe.model';
 export class RecipeDetailsComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {}
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(
+      this.selectedRecipe.ingredients
+    );
+  }
 }
